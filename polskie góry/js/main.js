dc65670739553ec350549773430f60d93ee44a00
marcinj8 <31106183+marcinj8@users.noreply.github.com>
2018-03-01 13:48:37 +0100

    Add files via upload

--- polskie góry/js/main.js
+++ polskie góry/js/main.js
@@ -11,6 +11,7 @@ $(window).scroll(function() {
     var $swietokrzyskie = $('#swietokrzyskie').offset().top - 200 ;
 
    
+   
     if ($position > 600 ) {
         $('#scrollUp').fadeIn();
         $('.sideMenu').fadeIn();
@@ -24,36 +25,50 @@ $(window).scroll(function() {
 
     if ($position > $tatry) {
         $('.sideMenu ul li').removeClass('active');
+        $('.sideMenu ul li').addClass('sideMenuItem');
+        $('.tatry').removeClass('sideMenuItem'); 
         $('.tatry').addClass('active');       
     }
 
     if ($position > $bieszczady) {
         $('.sideMenu ul li').removeClass('active');
+        $('.sideMenu ul li').addClass('sideMenuItem');
+        $('.bieszczady').removeClass('sideMenuItem'); 
         $('.bieszczady').addClass('active');       
     }
 
     if ($position > $karkonosze) {
         $('.sideMenu ul li').removeClass('active');
+        $('.sideMenu ul li').addClass('sideMenuItem');
+        $('.karkonosze').removeClass('sideMenuItem'); 
         $('.karkonosze').addClass('active');       
     }
 
     if ($position > $pieniny) {
         $('.sideMenu ul li').removeClass('active');
+        $('.sideMenu ul li').addClass('sideMenuItem');
+        $('.pieniny').removeClass('sideMenuItem'); 
         $('.pieniny').addClass('active');       
     }
 
     if ($position > $gorce) {
         $('.sideMenu ul li').removeClass('active');
+        $('.sideMenu ul li').addClass('sideMenuItem');
+        $('.gorce').removeClass('sideMenuItem'); 
         $('.gorce').addClass('active');       
     }
 
     if ($position > $beskid) {
         $('.sideMenu ul li').removeClass('active');
+        $('.sideMenu ul li').addClass('sideMenuItem');
+        $('.beskid').removeClass('sideMenuItem'); 
         $('.beskid').addClass('active');       
     }
 
     if ($position > $swietokrzyskie) {
         $('.sideMenu ul li').removeClass('active');
+        $('.sideMenu ul li').addClass('sideMenuItem');
+        $('.swietokrzyskie').removeClass('sideMenuItem'); 
         $('.swietokrzyskie').addClass('active');       
     }
 })
